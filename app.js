4bd4b4aeccf01a6ff77480afa0bbb053161f93c1
ibadhussain11 <hussainibad111@gmail.com>
2020-07-10 14:09:14 +0500

    test subject

---
+++ app.js
@@ -0,0 +1,64 @@
+// // var x = ["ali", "ibad", "saqlain", "shahzaib"];
+// // console.log(x[2],"is a good boy")
+
+// // var arr = new Array("2","3","34","5");
+// // console.log(arr);
+
+
+// // arr.append = "ali";
+// // console.log(arr);
+
+// // var k;
+// // for (k=1; k<=10 k++){
+// //     document.write(`${k}<br>`)
+// // }
+
+// // var i = 1;
+
+// // for (i; i <= 10; i++) {
+// //      document.write("2" + "x " + i + "=" + 2 * i + "<br>" + "<br>");
+// // }
+
+// // newFunction();
+
+// // function newFunction() {
+// //     var name = ["ali", "ibad", "saqlain", "aoun", "kinnu"];
+
+// //     for (var i = 0; i < name.length; i++) {
+// //         document.write(name[i]);
+// //     }
+// // }
+
+// var cities = ["karachi","punjab","islamabad","gujrat"]
+
+// for 
+
+
+newFunction()
+
+function newFunction() {
+    var x = prompt("enter your First value: ")
+    var y = prompt("enter your Second value: ")
+    var oper = prompt("enter your Operator: ")
+
+    if (oper === "+") {
+        console.log((+x) + (+y))
+
+    }
+    else if (oper === "-") {
+
+        console.log((+x) - (+y))
+    }
+    else if (oper === "*") {
+
+        console.log((+x) * (+y))
+    }
+    else if (oper === "/") {
+
+        console.log((+x) / (+y))
+    }
+    else if (oper === "%") {
+
+        console.log((+x) % (+y))
+    }
+}
